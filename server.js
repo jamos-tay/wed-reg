63702f6c09f67ab6227643f287a9c579b06ac4be
Jamos Tay <jamostay94@hotmail.com>
2017-12-26 17:49:32 +0800

    Store state and disconnect

--- server.js
+++ server.js
@@ -1,20 +1,10 @@
 function startServer(){
 	//process.env.EDGE_NATIVE = "./native/win32/ia32/4.1.1/edge_nativeclr";
-	var names = require('./names.json');
-	var sockets = [];
-	for (var property in names) {
-		if (names.hasOwnProperty(property)) {
-			var tnum = names[property];
-			names[property] = {
-				"tnum" : tnum,
-				"reg" : false
-			};
-		}
-	}
 	
 	const express = require('express');
 	const socketIO = require('socket.io');
 	const path = require('path');
+	const fs = require('fs');
 
 	const PORT = process.env.PORT || 3000;
 	const INDEX = path.join(__dirname, 'index.html');
@@ -31,28 +21,52 @@ function startServer(){
 
     server.use('/index_files', express.static('index_files'));
     server.use('/assets', express.static('assets'));
-
 	
-
+	var sockets = {};
+	var names;
+	if (fs.existsSync('./data.json')) {
+		names = require('./data.json');
+	} else{
+		names = require('./names.json');
+		for (var property in names) {
+			if (names.hasOwnProperty(property)) {
+				var tnum = names[property];
+				names[property] = {
+					"tnum" : tnum,
+					"reg" : false
+				};
+			}
+		}
+	}
+	
 
 	const io = socketIO(server.listen(PORT, () => console.log(`Listening on ${ PORT }`)));
 
-	var disseminate = function(){
-		for(var i = 0; i < sockets.length; i++){
-			sockets[i].emit('names', names);
+	var update = function(){
+		fs.writeFile('./data.json', JSON.stringify(names) , 'utf-8');
+		for (var socketID in sockets) {
+			if (sockets.hasOwnProperty(socketID)) {
+				sockets[socketID].emit('names', names);
+			}
 		}
 	};
 	
 	io.on('connection', function(socket){
-		sockets.push(socket);
+		console.log(socket.id + " connected");
+		sockets[socket.id] = socket;
 		socket.emit('names', names);
-			
+		
+		socket.on('disconnect', function() {
+			console.log(socket.id + " disconnected");
+			delete sockets[socket.id];
+		});
+		
 		socket.on('send', function(name){
 			if(!(name[0] in names)){
 				return;
 			}
 			names[name[0]].reg = name[1];
-			disseminate();
+			update();
 		});
 	
 	});
